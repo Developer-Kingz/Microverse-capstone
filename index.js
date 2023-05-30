75d4f2ff1232058c66c52126384b1a6307223d9a
Kingsley Igbor <chidoziek2@gmail.com>
2023-05-30 14:56:29 +0100

    fixed linters error

--- index.js
+++ index.js
@@ -1,97 +1,97 @@
-const openMenu = document.getElementById("menu-pop");
+const openMenu = document.getElementById('menu-pop');
 // const closemenu = document.getElementById("menu-close");
 
 function popMenu() {
-    openMenu.classList.add("pop-menu-show");
+  openMenu.classList.add('pop-menu-show');
 }
 popMenu();
 
 function closePop() {
-    openMenu.classList.remove("pop-menu-show");
+  openMenu.classList.remove('pop-menu-show');
 }
 closePop();
 
 const data = [
-    {
-        id: 1,
-        name: "SohYeong Noh",
-        position: "Director of Art center Nabi and a board member of CC Korea",
-        description:
-            "As a main venue for new media art production in Korea, Nabi promotes cross-discipline collaboration and understading among science technology, humanities and Art",
-        mobileImg: "Snapshoot Portfolio.svg",
-        desktopImg: "images/speaker_01 1 (3).svg",
-    },
-    {
-        id: 2,
-        name: "James Maddisson",
-        position: "Director of Art center Nabi and a board member of CC Korea",
-        description:
-            "As a main venue for new media art production in Korea, Nabi promotes cross-discipline collaboration and understading among science technology, humanities and Art",
-        mobileImg: "Snapshoot Portfolio.svg",
-        desktopImg: "images/speaker_01 1 (4).svg",
-    },
-    {
-        id: 3,
-        name: "Emperor Martins",
-        position: "Director of Art center Nabi and a board member of CC Korea",
-        description:
-            "As a main venue for new media art production in Korea, Nabi promotes cross-discipline collaboration and understading among science technology, humanities and Art",
-        mobileImg: "Snapshoot Portfolio.svg",
-        desktopImg: "images/speaker_01 1 (2).svg",
-    },
-    {
-        id: 4,
-        name: "Jeff Jury",
-        position: "Director of Art center Nabi and a board member of CC Korea",
-        description:
-            "As a main venue for new media art production in Korea, Nabi promotes cross-discipline collaboration and understading among science technology, humanities and Art",
-        mobileImg: "Snapshoot Portfolio.svg",
-        desktopImg: "images/speaker_01 1 (5).svg",
-    },
-    {
-        id: 5,
-        name: "Peter Shenk",
-        position: "Director of Art center Nabi and a board member of CC Korea",
-        description:
-            "As a main venue for new media art production in Korea, Nabi promotes cross-discipline collaboration and understading among science technology, humanities and Art",
-        mobileImg: "Snapshoot Portfolio.svg",
-        desktopImg: "images/speaker_01 1 (6).svg",
-    },
-    {
-        id: 6,
-        name: "Desmond liland",
-        position: "Director of Art center Nabi and a board member of CC Korea",
-        description:
-            "As a main venue for new media art production in Korea, Nabi promotes cross-discipline collaboration and understading among science technology, humanities and Art",
-        mobileImg: "Snapshoot Portfolio.svg",
-        desktopImg: "images/speaker_01 1 (7).svg",
-    },
+  {
+    id: 1,
+    name: 'SohYeong Noh',
+    position: 'Director of Art center Nabi and a board member of CC Korea',
+    description:
+            'As a main venue for new media art production in Korea, Nabi promotes cross-discipline collaboration and understading among science technology, humanities and Art',
+    mobileImg: 'Snapshoot Portfolio.svg',
+    desktopImg: 'images/speaker_01 1 (3).svg',
+  },
+  {
+    id: 2,
+    name: 'James Maddisson',
+    position: 'Director of Art center Nabi and a board member of CC Korea',
+    description:
+            'As a main venue for new media art production in Korea, Nabi promotes cross-discipline collaboration and understading among science technology, humanities and Art',
+    mobileImg: 'Snapshoot Portfolio.svg',
+    desktopImg: 'images/speaker_01 1 (4).svg',
+  },
+  {
+    id: 3,
+    name: 'Emperor Martins',
+    position: 'Director of Art center Nabi and a board member of CC Korea',
+    description:
+            'As a main venue for new media art production in Korea, Nabi promotes cross-discipline collaboration and understading among science technology, humanities and Art',
+    mobileImg: 'Snapshoot Portfolio.svg',
+    desktopImg: 'images/speaker_01 1 (2).svg',
+  },
+  {
+    id: 4,
+    name: 'Jeff Jury',
+    position: 'Director of Art center Nabi and a board member of CC Korea',
+    description:
+            'As a main venue for new media art production in Korea, Nabi promotes cross-discipline collaboration and understading among science technology, humanities and Art',
+    mobileImg: 'Snapshoot Portfolio.svg',
+    desktopImg: 'images/speaker_01 1 (5).svg',
+  },
+  {
+    id: 5,
+    name: 'Peter Shenk',
+    position: 'Director of Art center Nabi and a board member of CC Korea',
+    description:
+            'As a main venue for new media art production in Korea, Nabi promotes cross-discipline collaboration and understading among science technology, humanities and Art',
+    mobileImg: 'Snapshoot Portfolio.svg',
+    desktopImg: 'images/speaker_01 1 (6).svg',
+  },
+  {
+    id: 6,
+    name: 'Desmond liland',
+    position: 'Director of Art center Nabi and a board member of CC Korea',
+    description:
+            'As a main venue for new media art production in Korea, Nabi promotes cross-discipline collaboration and understading among science technology, humanities and Art',
+    mobileImg: 'Snapshoot Portfolio.svg',
+    desktopImg: 'images/speaker_01 1 (7).svg',
+  },
 ];
 
 const mobileData = [
-    {
-        id: 1,
-        name: "SohYeong Noh",
-        position: "Director of Art center Nabi and a board member of CC Korea",
-        description:
-            "As a main venue for new media art production in Korea, Nabi promotes cross-discipline collaboration and understading among science technology, humanities and Art",
-        mobileImg: "images/speaker_01 1.svg",
-    },
-    {
-        id: 2,
-        name: "James Maddisson",
-        position: "Director of Art center Nabi and a board member of CC Korea",
-        description:
-            "As a main venue for new media art production in Korea, Nabi promotes cross-discipline collaboration and understading among science technology, humanities and Art",
-        mobileImg: "images/speaker_01 1 (1).svg",
-    },
+  {
+    id: 1,
+    name: 'SohYeong Noh',
+    position: 'Director of Art center Nabi and a board member of CC Korea',
+    description:
+            'As a main venue for new media art production in Korea, Nabi promotes cross-discipline collaboration and understading among science technology, humanities and Art',
+    mobileImg: 'images/speaker_01 1.svg',
+  },
+  {
+    id: 2,
+    name: 'James Maddisson',
+    position: 'Director of Art center Nabi and a board member of CC Korea',
+    description:
+            'As a main venue for new media art production in Korea, Nabi promotes cross-discipline collaboration and understading among science technology, humanities and Art',
+    mobileImg: 'images/speaker_01 1 (1).svg',
+  },
 ];
 
 data.forEach((item) => {
-    const section = document.createElement("div");
-    section.className = "speaker2";
+  const section = document.createElement('div');
+  section.className = 'speaker2';
 
-    section.innerHTML = `
+  section.innerHTML = `
      <img src="${item.desktopImg}" alt="" />
      <div class="speaker2-write">
         <h3>${item.name}</h3>
@@ -105,15 +105,15 @@ data.forEach((item) => {
         </div>
     `;
 
-    const speakers = document.querySelector(".desktop-speakers");
-    speakers.appendChild(section);
+  const speakers = document.querySelector('.desktop-speakers');
+  speakers.appendChild(section);
 });
 
 mobileData.forEach((item) => {
-    const mobileSection = document.createElement("div");
-    mobileSection.className = "speaker1";
+  const mobileSection = document.createElement('div');
+  mobileSection.className = 'speaker1';
 
-    mobileSection.innerHTML = `
+  mobileSection.innerHTML = `
     <img src="${item.mobileImg}" alt="" />
     <div class="speaker1-write">
         <h3>${item.name}</h3>
@@ -127,15 +127,6 @@ mobileData.forEach((item) => {
     </div>
     `;
 
-    const mobileSpeakers = document.querySelector(".featured-speakers");
-    mobileSpeakers.appendChild(mobileSection);
+  const mobileSpeakers = document.querySelector('.featured-speakers');
+  mobileSpeakers.appendChild(mobileSection);
 });
-
-// const moreButton = document.createElement("div");
-// moreButton.className = "more-speakers";
-
-// moreButton.innerHTML = `
-//     More <img src="images/arrow_down 1.svg" alt="" />
-//     `;
-
-// mobileSpeakers.appendChild(moreButton);
